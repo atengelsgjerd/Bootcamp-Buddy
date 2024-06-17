fe29a5b61f7bf107cf78d72a995ea0bcc42f1394
atengelsgjerd <atengelsgjerd@gmail.com>
2024-06-17 10:37:41 -0500

    seeded database and altered stylings

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,31 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'postgres',
-        }
-    );
+// const sequelize = process.env.DB_URL
+//     ? new Sequelize(process.env.DB_URL)
+//     : new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASSWORD,
+//         {
+//             host: 'localhost',
+//             dialect: 'postgres',
+//         }
+//     );
+const database = process.env.DB_NAME;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const sequelize = new Sequelize(database, username, password, {
+    dialect: 'postgres',
+    host: 'dpg-cpl59fn109ks73dmh440-a.oregon-postgres.render.com',
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // You may need to set this to false for self-signed certificates
+      }
+    }
+  });
 
     module.exports = sequelize;
+
--- public/js/login.js
+++ public/js/login.js
@@ -1,44 +1,44 @@
-const loginFormFunction = async (event) => {
-    event.preventDefault();
+// const loginFormFunction = async (event) => {
+//     event.preventDefault();
 
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({email, password}),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.assign('/');
-            console.log('logged in');
-        } else {
-            alert('Failed to log in');
+//     const email = document.querySelector('#email').value.trim();
+//     const password = document.querySelector('#password').value.trim();
+//     if (email && password) {
+//         const response = await fetch('/api/users/login', {
+//             method: 'POST',
+//             body: JSON.stringify({email, password}),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+//         if (response.ok) {
+//             document.location.assign('/');
+//             console.log('logged in');
+//         } else {
+//             alert('Failed to log in');
+//         }
+//     }
+// };
+
+// document
+//     .querySelector('#loginBtn')
+//     .addEventListener('click', loginFormFunction);
+
+    const loginFormFunction = async (event) => {
+        console.log('login form function');
+        event.preventDefault();
+    
+        const email = document.querySelector('#email').value.trim();
+        const password = document.querySelector('#password').value.trim();
+        if (email && password) {
+            const response = await axios.post('/api/users/login', {email, password});   
+            if (response.status === 200) {
+                document.location.assign('/');
+                console.log('logged in');
+            } else {
+                alert('Failed to log in');
+            }
         }
-    }
-};
-
-document
-    .querySelector('#loginBtn')
-    .addEventListener('click', loginFormFunction);
-
-    // const loginFormFunction = async (event) => {
-    //     console.log('login form function');
-    //     event.preventDefault();
+    };
     
-    //     const email = document.querySelector('#email').value.trim();
-    //     const password = document.querySelector('#password').value.trim();
-    //     if (email && password) {
-    //         const response = await axios.post('/api/users/login', {email, password});   
-    //         if (response.status === 200) {
-    //             document.location.assign('/');
-    //             console.log('logged in');
-    //         } else {
-    //             alert('Failed to log in');
-    //         }
-    //     }
-    // };
-    
-    // document
-    //     .querySelector('#loginBtn')
-    //     .addEventListener('click', loginFormFunction);
+    document
+        .querySelector('#loginBtn')
+        .addEventListener('click', loginFormFunction);
