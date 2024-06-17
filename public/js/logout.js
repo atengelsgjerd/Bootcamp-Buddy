2139230bb38eb609c6b48572350c9e37d4d4d1fa
atengelsgjerd <atengelsgjerd@gmail.com>
2024-06-17 11:00:49 -0500

    fixed logout route

--- public/js/logout.js
+++ public/js/logout.js
@@ -14,7 +14,7 @@
 
 const logout = async () => {
     try {
-        const response = await axios.post('/logout', {
+        const response = await axios.post('/api/users/logout', {
             headers: { 'Content-Type': 'application/json' }
         });
         if (response.status === 200) {
