13a67b61f29e87393be1a3a9cd3b95c60cf52605
atengelsgjerd <atengelsgjerd@gmail.com>
2024-06-17 10:47:09 -0500

    updated public logout and signup routes

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,31 +1,31 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    });
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to log out');
-    }
-};
-
-document.getElementById('logout').addEventListener('click', logout);
-
 // const logout = async () => {
-//     try {
-//         const response = await axios.post('/logout', {
-//             headers: { 'Content-Type': 'application/json' }
-//         });
-//         if (response.status === 200) {
-//             document.location.replace('/');
-//         } else {
-//             alert('Failed to log out');
-//         }
-//     } catch (error) {
-//         console.error('An error occurred while logging out:', error);
+//     const response = await fetch('/api/users/logout', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' }
+//     });
+//     if (response.ok) {
+//         document.location.replace('/');
+//     } else {
 //         alert('Failed to log out');
 //     }
 // };
 
 // document.getElementById('logout').addEventListener('click', logout);
+
+const logout = async () => {
+    try {
+        const response = await axios.post('/logout', {
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.status === 200) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log out');
+        }
+    } catch (error) {
+        console.error('An error occurred while logging out:', error);
+        alert('Failed to log out');
+    }
+};
+
+document.getElementById('logout').addEventListener('click', logout);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,48 @@
-const signUpButtonEl = document.getElementById('submitBtn');
+// const signUpButtonEl = document.getElementById('submitBtn');
 
-async function signUpFunction(e){
-    e.preventDefault();
-    const usernameEl = document.getElementById('username').value.trim();
-    const emailEl = document.getElementById('signUpEmail').value.trim();
-    const passwordEl = document.getElementById('signUpPassword').value.trim();
+// async function signUpFunction(e){
+//     e.preventDefault();
+//     const usernameEl = document.getElementById('username').value.trim();
+//     const emailEl = document.getElementById('signUpEmail').value.trim();
+//     const passwordEl = document.getElementById('signUpPassword').value.trim();
    
     
-    if (usernameEl && emailEl && passwordEl) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({username: usernameEl, email: emailEl, password: passwordEl}),
-            headers: { 'Content-Type' : 'application/json' },
+//     if (usernameEl && emailEl && passwordEl) {
+//         const response = await fetch('/api/users', {
+//             method: 'POST',
+//             body: JSON.stringify({username: usernameEl, email: emailEl, password: passwordEl}),
+//             headers: { 'Content-Type' : 'application/json' },
 
-        });
+//         });
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to sign up');
+//     if (response.ok) {
+//         document.location.replace('/');
+//     } else {
+//         alert('Failed to sign up');
+//     }
+// }
+
+// };
+
+// signUpButtonEl.addEventListener('click', signUpFunction);
+
+const signUpFormFunction = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#signUpEmail').value.trim();
+    const password = document.querySelector('#signUpPassword').value.trim();
+    if (username && email && password) {
+        const response = await axios.post('/api/users', {username, email, password});   
+        if (response.status === 200) {
+            document.location.assign('/');
+            console.log('logged in');
+        } else {
+            alert('Failed to log in');
+        }
     }
 }
 
-};
-
-signUpButtonEl.addEventListener('click', signUpFunction);
+document
+    .querySelector('#submitBtn')
+    .addEventListener('click', signUpFormFunction);
